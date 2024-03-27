664d27df7e1fff606a8c14a40952262d77d7a791
Arth Mohan <writetoarth@gmail.com>
2024-03-27 22:27:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.dropdown').click(function() {
+        $(this).find('.dropdown-content').toggle(); // Toggle the display of dropdown content on click
+    });
+});
